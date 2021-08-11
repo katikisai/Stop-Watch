19c2dfb6a6c1af6b2f45daf7f2bfd7275cd82ad3
katikisai <saiprakash.katiki@gmail.com>
2021-08-11 19:41:05 +0530

    changed

--- stopwatch.js
+++ stopwatch.js
@@ -6,7 +6,7 @@ var milliseconds = 0 ;
 var startTimer = function() {
     millisecInt = setInterval(function(){
         milliseconds = ( milliseconds > 1000 ) ? 1 : ( milliseconds + 1 ) ;
-        setTimeToHTML(hours, minutes, seconds, milliseconds);
+        setTime(hours, minutes, seconds, milliseconds);
     },1);
     secondsInt = setInterval(function() {
         seconds = ( seconds > 59 ) ? 1 : ( seconds + 1 ) ;
@@ -28,11 +28,11 @@ var stopTimer = function() {
 var resetTimer = function() {
     stopTimer();
     hours = 0 ; minutes = 0 ; seconds = 0 ; milliseconds = 0 ;
-    setTimeToHTML(hours, minutes, seconds, milliseconds);
+    setTime(hours, minutes, seconds, milliseconds);
     document.getElementById("start").disabled = false;
 }
 
-var setTimeToHTML = function(hours, minutes, seconds, milliseconds) {
+var setTime = function(hours, minutes, seconds, milliseconds) {
     document.getElementById("hours").innerText = (hours.toString().length == 1) ? ("0" + hours) : hours ;
     document.getElementById("minutes").innerText = (minutes.toString().length == 1) ? ("0" + minutes) : minutes ;
     document.getElementById("seconds").innerText = (seconds.toString().length == 1) ? ("0" + seconds) : seconds ;
